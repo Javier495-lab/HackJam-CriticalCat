fb20b887fa85815f704b2bb1551d1dae33776146
elguay <dario.mart.lop.03@gmail.com>
2026-09-06 00:26:41 +0200

    Added infinite scroll floor

---
+++ Assets/Scripts/Misc/InfiniteFloor.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class InfiniteFloor : MonoBehaviour
+{
+	[SerializeField] private float scrollVelocity;
+	[SerializeField] private Material myMat;
+
+	private void Start()
+	{
+		myMat = GetComponent<Renderer>().material;
+	}
+	private void Update()
+	{
+		UpdateScrollVelocity();
+	}
+
+	private void UpdateScrollVelocity()
+	{
+		scrollVelocity = LevelManager.Instance.GetCurrentLevelVelocity();
+		myMat.SetFloat("_ScrollVelocity", scrollVelocity / 5);
+	}
+}
